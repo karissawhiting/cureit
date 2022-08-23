dde0c0a601706b53f73f4401b8c73e515ef4cff8
Fei <feit1@mskcc.org>
2022-08-23 11:27:40 -0400

    Fix a bug in tidy function

--- R/broom_methods.R
+++ R/broom_methods.R
@@ -4,7 +4,6 @@
 #' coefficient estimates. Defaults to `FALSE`.
 #' @param conf.level Level of the confidence interval. Default matches that in
 #' `smcure(conf.level=)` (typically, 0.95)
-#' @inheritParams broom::tidy.glm
 #'
 #' @name broom_methods_smcure
 #' @return a tibble
@@ -24,22 +23,30 @@ tidy.smcure <- function(x,
                         conf.int = FALSE,
                         conf.level = 0.95, ...) {
   
-  df_logit <- broom::tidy(
-    x$logistfit,
-    exponentiate = exponentiate,
-    conf.int = conf.int,
-    conf.level = conf.level, ...
-  )
-  
-  if (isTRUE(conf.int)) {
-    df_logit <-
-      df_logit %>%
-      dplyr::relocate(.data$conf.low, .data$conf.high, .before = .data$p.value)
-  }
-  
-  df_logit$term <- paste0(x$bnm,", Cure model")
+  # df_logit <- broom::tidy(
+  #   x$logistfit,
+  #   exponentiate = exponentiate,
+  #   conf.int = conf.int,
+  #   conf.level = conf.level, ...
+  # )
+  # 
+  # if (isTRUE(conf.int)) {
+  #   df_logit <-
+  #     df_logit %>%
+  #     dplyr::relocate(.data$conf.low, .data$conf.high, .before = .data$p.value)
+  # }
+  # 
+  # df_logit$term <- paste0(x$bnm,", Cure model")
   
   if (conf.int){
+    df_logit <- data.frame(term = paste0(x$bnm,", Cure model"),
+                           estimate=x$b,
+                           std.error=x$b_sd,
+                           statistic=x$b_zvalue,
+                           conf.low=x$b-1.96*x$b_sd,
+                           conf.high=x$b+1.96*x$b_sd,
+                           p.value=x$b_pvalue) %>% as_tibble()
+    
     df_surv <- data.frame(term=paste0(x$betanm,", Survival model"),
                           estimate=x$beta,
                           std.error=x$beta_sd,
@@ -49,15 +56,23 @@ tidy.smcure <- function(x,
                           p.value=x$beta_pvalue
     ) %>% as_tibble() 
     if (exponentiate){
+      df_logit[,c(2,5,6)] <- exp(df_logit[,c(2,5,6)])
       df_surv[,c(2,5,6)] <- exp(df_surv[,c(2,5,6)])
     }
   }else{
+    df_logit <- data.frame(term = paste0(x$bnm,", Cure model"),
+                           estimate=x$b,
+                           std.error=x$b_sd,
+                           statistic=x$b_zvalue,
+                           p.value=x$b_pvalue) %>% as_tibble()
+    
     df_surv <- data.frame(term=paste0(x$betanm,", Survival model"),
                           estimate=x$beta,
                           std.error=x$beta_sd,
                           statistic=x$beta_zvalue,
                           p.value=x$beta_pvalue) %>% as_tibble() 
     if (exponentiate){
+      df_logit[,2] <- exp(df_logit[,2])
       df_surv[,2] <- exp(df_surv[,2])
     }
   }
@@ -67,14 +82,12 @@ tidy.smcure <- function(x,
   
 }
 
-
 #' Broom methods for cureit objects
 #'
 #' @param exponentiate Logical indicating whether or not to exponentiate the
 #' coefficient estimates. Defaults to `FALSE`.
 #' @param conf.level Level of the confidence interval. Default matches that in
-#' `cureit(conf.level=)` (typically, 0.95)
-#' @inheritParams broom::tidy.glm
+#' `smcure(conf.level=)` (typically, 0.95)
 #'
 #' @name broom_methods_cureit
 #' @return a tibble
@@ -92,24 +105,32 @@ NULL
 tidy.cureit <- function(x,
                         exponentiate = FALSE,
                         conf.int = FALSE,
-                        conf.level = x$conf.level, ...) {
+                        conf.level = 0.95, ...) {
   
-  df_logit <- broom::tidy(
-    x$smcure$logistfit,
-    exponentiate = exponentiate,
-    conf.int = conf.int,
-    conf.level = conf.level, ...
-  )
-  
-  if (isTRUE(conf.int)) {
-    df_logit <-
-      df_logit %>%
-      dplyr::relocate(.data$conf.low, .data$conf.high, .before = .data$p.value)
-  }
-  
-  df_logit$term <- paste0(x$smcure$bnm,", Cure model")
+  # df_logit <- broom::tidy(
+  #   x$logistfit,
+  #   exponentiate = exponentiate,
+  #   conf.int = conf.int,
+  #   conf.level = conf.level, ...
+  # )
+  # 
+  # if (isTRUE(conf.int)) {
+  #   df_logit <-
+  #     df_logit %>%
+  #     dplyr::relocate(.data$conf.low, .data$conf.high, .before = .data$p.value)
+  # }
+  # 
+  # df_logit$term <- paste0(x$bnm,", Cure model")
   
   if (conf.int){
+    df_logit <- data.frame(term = paste0(x$smcure$bnm,", Cure model"),
+                           estimate=x$smcure$b,
+                           std.error=x$smcure$b_sd,
+                           statistic=x$smcure$b_zvalue,
+                           conf.low=x$smcure$b-1.96*x$smcure$b_sd,
+                           conf.high=x$smcure$b+1.96*x$smcure$b_sd,
+                           p.value=x$smcure$b_pvalue) %>% as_tibble()
+    
     df_surv <- data.frame(term=paste0(x$smcure$betanm,", Survival model"),
                           estimate=x$smcure$beta,
                           std.error=x$smcure$beta_sd,
@@ -119,18 +140,28 @@ tidy.cureit <- function(x,
                           p.value=x$smcure$beta_pvalue
     ) %>% as_tibble() 
     if (exponentiate){
+      df_logit[,c(2,5,6)] <- exp(df_logit[,c(2,5,6)])
       df_surv[,c(2,5,6)] <- exp(df_surv[,c(2,5,6)])
     }
   }else{
+    df_logit <- data.frame(term = paste0(x$smcure$bnm,", Cure model"),
+                           estimate=x$smcure$b,
+                           std.error=x$smcure$b_sd,
+                           statistic=x$smcure$b_zvalue,
+                           p.value=x$smcure$b_pvalue) %>% as_tibble()
+    
     df_surv <- data.frame(term=paste0(x$smcure$betanm,", Survival model"),
                           estimate=x$smcure$beta,
                           std.error=x$smcure$beta_sd,
                           statistic=x$smcure$beta_zvalue,
                           p.value=x$smcure$beta_pvalue) %>% as_tibble() 
     if (exponentiate){
+      df_logit[,2] <- exp(df_logit[,2])
       df_surv[,2] <- exp(df_surv[,2])
     }
   }
   
   list(df_cure=df_logit,
-       df_surv=df_surv)}
+       df_surv=df_surv)
+  
+}
